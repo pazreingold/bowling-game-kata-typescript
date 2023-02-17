b3e5f0fd6787490083fa1865beb46277852a01b1
pazrein <pazreingold1996@gmail.com>
2023-02-17 19:12:18 +0200

    Refactor - Extract to Functions

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,26 +4,31 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
-  const range = (start, end) =>
-    Array.from({ length: end - start }, (v, k) => k + start);
-
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService]
+      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
+  function rollMany(rolls: number, pins: number): void {
+    range(0, rolls).forEach(() => appController.roll(pins));
+  }
+
+  function range(start: number, end: number): number[] {
+    return Array.from({ length: end - start }, (v, k) => k + start);
+  }
+
   describe('root', () => {
     it('game of all zeros', () => {
-      range(0, 20).forEach(() => appController.roll(0));
+      rollMany(20, 0);
       expect(appController.score()).toBe(0);
     });
 
     it('game of all ones', () => {
-      range(0, 20).forEach(() => appController.roll(1));
+      rollMany(20, 1);
       expect(appController.score()).toBe(20);
     });
   });
